b1eec52b3a40fd0cb4389d91d20a6b077317b7c1
“gos0808” <gos08.08.23@gmail.com>
2024-07-16 22:35:56 +0400

    changed styles

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -22,7 +22,7 @@ export const About = () => {
                 <p>Monday - Friday: 9:00 am - 5:00 pm</p>
                 <p>Saturday - Sunday: 9:00 am - 9:00 pm</p>                         <p>Public Holidays: 9:00 am - 9:00 pm</p>
 
-                <h1>Contact us</h1>
+                <h1 className='second-header'>Contact us</h1>
                 <p>Email: contact@blue-sky-farm.com</p>
                 <p>Phone: + 1(111)111-1111</p>
             </div>
