94849cdfaf610d8cb56456c5c08a16196b03b0a3
“gos0808” <gos08.08.23@gmail.com>
2024-07-21 15:07:04 +0400

    added new data files

---
+++ src/Data/products.js
@@ -0,0 +1,256 @@
+export const productsData = [
+    {
+        name: 'Cucumber',
+        category: 'Vegetables',
+        price: 5.00,
+        newPrice: 2.99,
+        weight: '400g',
+        season: 'July, August, September',
+        img: 'https://images.unsplash.com/photo-1449300079323-02e209d9d3a6?q=80&w=2274&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Sweet Peppers',
+        category: 'Vegetables',
+        price: 3.50,
+        newPrice: null,
+        weight: '400g',
+        season: 'July, August, September, October',
+        img: 'https://images.unsplash.com/photo-1592801062201-04fd6cf3d5ed?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Garlic',
+        category: 'Vegetables',
+        price: 3.75,
+        newPrice: 2.00,
+        weight: '70g',
+        season: 'June, July, August, September, October, November',
+        img: 'https://images.unsplash.com/photo-1579705744772-f26014b5e084?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Asparagus',
+        category: 'Vegetables',
+        price: 5.50,
+        newPrice: null,
+        weight: '455g',
+        season: 'April, May, June',
+        img: 'https://images.unsplash.com/photo-1562068609-d1588b747e3e?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Strawberries',
+        category: 'Berries',
+        price: 5.50,
+        newPrice: null,
+        weight: '227g',
+        season: 'June, July, August',
+        img: 'https://images.unsplash.com/photo-1610725664338-2be2cb450798?q=80&w=465&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Kale',
+        category: 'Greens',
+        price: 3.00,
+        newPrice: 2.75,
+        weight: '100g',
+        season: 'June, July, August, September, October, November',
+        img: 'https://images.unsplash.com/photo-1524179091875-bf99a9a6af57?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Green Onions',
+        category: 'Greens',
+        price: 2.25,
+        newPrice: 2.00,
+        weight: '75g',
+        season: 'July, August, September, October, November',
+        img: 'https://images.unsplash.com/photo-1602769515559-e15133a7e992?q=80&w=709&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Tomato',
+        category: 'Vegetables',
+        price: 4.75,
+        newPrice: null,
+        weight: '375g',
+        season: 'July, August, September, October',
+        img: 'https://images.unsplash.com/photo-1562695530-ca03c4b98623?q=80&w=326&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Eggplant',
+        category: 'Vegetables',
+        price: 3.75,
+        newPrice: null,
+        weight: '250g',
+        season: 'July, August, September, October',
+        img: 'https://plus.unsplash.com/premium_photo-1666270423836-864dfa7071e5?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Basil',
+        category: 'Greens',
+        price: 3.25,
+        newPrice: null,
+        weight: '50g',
+        season: 'July, August, September, October',
+        img: 'https://images.unsplash.com/photo-1500420254515-0faefa2dac99?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Mint',
+        category: 'Greens',
+        price: 5.75,
+        newPrice: 4.75,
+        weight: '50g',
+        season: 'April, May, June, July, August, September, October',
+        img: 'https://images.unsplash.com/photo-1588908933351-eeb8cd4c4521?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Broccoli Microgreens',
+        category: 'Greens',
+        price: 2.75,
+        newPrice: null,
+        weight: '50g',
+        season: 'July, August, September, October, November',
+        img: 'https://images.unsplash.com/photo-1647613233077-c323b019968b?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+
+    {
+        name: 'Radish Microgreens',
+        category: 'Greens',
+        price: 2.5,
+        newPrice: null,
+        weight: '50g',
+        season: 'April, May, June, July, August, September, October',
+        img: 'https://plus.unsplash.com/premium_photo-1661636013858-dd91fd47ff92?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'White Mushrooms',
+        category: 'Mushrooms',
+        price: 6.00,
+        newPrice: 5.25,
+        weight: '250g',
+        season: 'April, May, June, July, August, September, October',
+        img: 'https://images.unsplash.com/photo-1583328439472-8130d925d63c?q=80&w=924&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Parsley',
+        category: 'Greens',
+        price: 5.25,
+        newPrice: 5.75,
+        weight: '30g',
+        season: 'June, July, August, September, October, November',
+        img: 'https://images.unsplash.com/photo-1590759485418-90509afec818?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Spinach',
+        category: 'Greens',
+        price: 5.75,
+        newPrice: null,
+        weight: '142g',
+        season: 'June, July',
+        img: 'https://images.unsplash.com/photo-1574316071802-0d684efa7bf5?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Brown Mushrooms',
+        category: 'Mushrooms',
+        price: 6.15,
+        newPrice: null,
+        weight: '250g',
+        season: 'April, May, June, July, August, September, October',
+        img: 'https://images.unsplash.com/photo-1504545102780-26774c1bb073?q=80&w=423&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Sweet Potatoes',
+        category: 'Vegetables',
+        price: 4.25,
+        newPrice: null,
+        weight: '908g',
+        season: 'August, September, October',
+        img: 'https://images.unsplash.com/photo-1680472628312-9ff2605ee718?q=80&w=465&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Rosemary',
+        category: 'Greens',
+        price: 5.75,
+        newPrice: 4.75,
+        weight: '30g',
+        season: 'April, May, June, July, August, September, October',
+        img: 'https://images.unsplash.com/photo-1603129624917-3c579e864025?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Oregano',
+        category: 'Greens',
+        price: 5.75,
+        newPrice: null,
+        weight: '30g',
+        season: 'May, June, July, August, September, October',
+        img: 'https://images.unsplash.com/photo-1652556532650-f711fa97d090?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Gala Apples',
+        category: 'Fruits',
+        price: 4.75,
+        newPrice: null,
+        weight: '908g',
+        season: 'May, June, July, August, September',
+        img: 'https://images.unsplash.com/photo-1502027198647-4f28359d9a17?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Empire Apples',
+        category: 'Fruits',
+        price: 12.5,
+        newPrice: 10,
+        weight: '2270g',
+        season: 'September, October',
+        img: 'https://images.unsplash.com/photo-1501925654609-7b41cf395eb8?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Golden Apples',
+        category: 'Fruits',
+        price: 4.75,
+        newPrice: null,
+        weight: '908g',
+        season: 'September, October',
+        img: 'https://images.unsplash.com/photo-1603086175742-bc683b0d2716?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Wild Blueberries',
+        category: 'Berries',
+        price: 5.75,
+        newPrice: null,
+        weight: '454g',
+        season: 'May, June, July, August, September',
+        img: 'https://images.unsplash.com/photo-1596043858186-7e7c8d4f84d1?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Cranberries',
+        category: 'Berries',
+        price: 4.50,
+        newPrice: null,
+        weight: '454g',
+        season: 'September, October',
+        img: 'https://images.unsplash.com/photo-1692815464877-3a55819caac9?q=80&w=386&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Carrots',
+        category: 'Vegetables',
+        price: 4.00,
+        newPrice: null,
+        weight: '1000g',
+        season: 'July, August, September, October, November',
+        img: 'https://images.unsplash.com/photo-1522184216316-3c25379f9760?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Cilantro',
+        category: 'Greens',
+        price: 5.75,
+        newPrice: 5.00,
+        weight: '30g',
+        season: 'June, July, August, September',
+        img: 'https://images.unsplash.com/photo-1535189487909-a262ad10c165?q=80&w=1999&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Dill',
+        category: 'Greens',
+        price: 3.5,
+        newPrice: 2.80,
+        weight: '30g',
+        season: 'June, July, August, September, October',
+        img: 'https://images.unsplash.com/photo-1636154851820-c3cbc74d232d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    }
+
+];
